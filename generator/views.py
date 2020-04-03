96c4c527695df61d12a42735da48c6de29031c34
Andrew <dev.andrewhan@gmail.com>
2020-04-02 21:08:04 -0400

    password generator

---
+++ generator/views.py
@@ -0,0 +1,33 @@
+import random
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+	return render(request, 'generator/home.html')
+
+def about(request):
+	return render(request, 'generator/about.html')
+
+def password(request):
+	characters = list('abcdefghijklmnopqrstuvwxyz')
+
+	if request.GET.get('uppercase'):
+		characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+	if request.GET.get('special'):
+		characters.extend(list('!@#$%^&*()'))
+
+	if request.GET.get('numbers'):
+		characters.extend(list('1234567890'))
+
+	length = int(request.GET.get('length', 12))
+
+	thepassword = ''
+	for x in range(length):
+		thepassword += random.choice(characters)
+
+	return render(request, 'generator/password.html', {'password': thepassword})
